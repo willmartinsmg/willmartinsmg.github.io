bd091ce13769868213be5f1af0750fdae5fcd0e1
William Martins <william@WMartins.local>
2019-11-15 19:32:33 -0300

    Tailwindcss funcioando

--- src/pages/about.js
+++ src/pages/about.js
@@ -6,7 +6,7 @@ const AboutPage = () => (
     <div>
       <Link to="/">Homepage</Link>
       <h1>About the Author</h1>
-      <h1 className="text-4xl font-bold">Hello World!</h1>
+      <h1 className="text-4xl font-bold text-center">Hello World!</h1>
       <p>Welcome to my Gatsby site.</p>
     </div>
   </main>
